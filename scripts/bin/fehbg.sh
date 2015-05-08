8970f0351b8bf185df861eea8008d414d52b3caf
Joe Schlimmer <joseph.schlimmer@gmail.com>
2015-05-07 23:50:55 -0400

    scripts hash bang to /usr/bin/env. Tmux updates

--- scripts/bin/fehbg.sh
+++ scripts/bin/fehbg.sh
@@ -1,4 +1,4 @@
-#/bin/zsh
+#/usr/bin/env bash
 #executes feh to draw the wallpaper, called from .i3/config
 
 #feh  --bg-scale '/home/joe/.wallpapers/oscar.jpg' '/home/joe/.wallpapers/white.png' '/home/joe/.wallpapers/white.png' 
